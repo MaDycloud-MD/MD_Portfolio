971ad266a638161a69feb16cfcee9d8fdcc24d0e
MaDycloud-MD <md.shoaib.i.makandar@gmail.com>
2025-06-23 18:40:19 +0530

    Resume update - Nav Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -98,7 +98,7 @@ export default function Navbar({ toggleTheme, isDark }) {
             ))}
             <li>
               <a
-                href="/Resume.pdf"
+                href="./Resume.pdf"
                 target="_blank"
                 className="bg-primary text-black px-3 py-1 rounded hover:bg-yellow-400 font-semibold transition inline-block"
               >
